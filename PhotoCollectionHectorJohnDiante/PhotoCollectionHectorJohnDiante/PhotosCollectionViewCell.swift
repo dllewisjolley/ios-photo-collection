aef94b1ccb5fbf134a314549934758c2fe1906a0
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-02 15:08:38 -0700

    Storyboard scene setup

---
+++ PhotoCollectionHectorJohnDiante/PhotoCollectionHectorJohnDiante/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollectionHectorJohnDiante
+//
+//  Created by Hector Steven on 5/2/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
