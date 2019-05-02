ecefad951588d6341f57ebbdd94d27f8c10250a1
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-02 15:35:04 -0700

    ThemeSelectionViewController

---
+++ PhotoCollectionHectorJohnDiante/PhotoCollectionHectorJohnDiante/ThemeSelectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollectionHectorJohnDiante
+//
+//  Created by Hector Steven on 5/2/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	@IBAction func yellowButtonPressed(_ sender: Any) {
+	}
+	@IBAction func darkButtonPressed(_ sender: Any) {
+	}
+	
+}
